dfbc6bf1b8904eed0d8ce54c3f59ececa1189c57
CanaanGM <reakak1@gmail.com>
2023-02-28 22:58:03 +0300

    sending functionality

--- cardDispenser/main.py
+++ cardDispenser/main.py
@@ -1,6 +1,23 @@
 from fastapi import FastAPI
+import pika
+from events import Events
+import time, threading
+
 
 app = FastAPI()
+SENDING = False
+sending = Events()
+
+
+def send_message():
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters("localhost")
+    )  # change when compose to app name
+    channel = connection.channel()
+    channel.queue_declare(queue="YuGiOh-Card-Queue")  # create queue if !queue
+    channel.basic_publish("", "YuGiOh-Card-Queue", '{"card-TEST": {"name": "test"}}')
+    # print("[x] Sent message")
+    connection.close()
 
 
 @app.get("/")
@@ -8,11 +25,27 @@ def handle_root():
     return {"endpoints": {"/start": "starts pumping", "/stop": "stops pumping"}}
 
 
+def change_state(state: bool):
+    sending.on_change += set_sending_state
+    sending.on_change(state)
+
+
+def set_sending_state(state: bool):
+    global SENDING
+    SENDING = state
+    while SENDING:
+        send_message()
+        time.sleep(10)
+
+
 @app.post("/start")
-def handle_post():
-    return "pumping"
+async def handle_post():
+    threading.Thread(target=change_state, args=(True,)).start()
+    # inform the requester that we are sending
+    return "sending"
 
 
 @app.post("/stop")
-def handle_stop():
+async def handle_stop():
+    threading.Thread(target=change_state, args=(False,)).start()
     return "stoping"
