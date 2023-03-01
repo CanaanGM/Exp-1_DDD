ce7e55e315337c957effe6c388ddb04598e5e10f
CanaanGM <reakak1@gmail.com>
2023-03-01 05:43:48 +0300

    C# client and preoducer

---
+++ CardProcessor/Processor/Program.cs
@@ -0,0 +1,106 @@
+﻿using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+using Processor;
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace YuGiOh_CMD_server
+{
+    internal class Program
+    {
+        public static bool Processing { get; set; } = false;
+
+        public static void Main(string[] args)
+        {
+            Run();
+        }
+
+        public async static void Run()
+        {
+            while (true)
+            {
+                switch (Console.ReadLine())
+                {
+                    case "Start":
+                        Task.Run(async () => await HandleStart());
+                        break;
+                    case "Stop":
+                        HandleStop();
+                        break;
+                    default:
+                        Console.WriteLine("awww");
+                        break;
+                }
+            }
+        }
+        private static void SendMessage(YuGiOhCard recievedCard)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "YuGiOh-Processed-Queue", false, false, false, null);
+            string message = JsonSerializer.Serialize(recievedCard);
+             var body = Encoding.UTF8.GetBytes(message);
+
+          channel.BasicPublish(
+              exchange: string.Empty, 
+              routingKey: "YuGiOh-Processed-Queue", 
+              basicProperties: null, 
+              body
+              );
+        }
+
+        private static void ConsumeMessage(IModel channel)
+        {
+            channel.QueueDeclare(
+                    queue: "YuGiOh-Card-Queue",false,false,false,null
+                );
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += Consumer_Received;
+
+            channel.BasicConsume(queue: "YuGiOh-Card-Queue",
+                     autoAck: true,
+                     consumer: consumer);
+        }
+
+        private static void Consumer_Received(object? sender, BasicDeliverEventArgs e)
+        {
+            var body = e.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+
+            SendMessage(JsonSerializer.Deserialize<YuGiOhCard>(message));
+        }
+
+        async public static Task HandleStart()
+        {
+            Processing = true;
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            while (Processing)
+            {
+                try
+                {
+                    ConsumeMessage(channel);
+                    
+                    if (!Processing) break;
+                }
+                catch (Exception ex)
+                {
+                    Console.Write(ex);
+                }
+            }
+            Console.WriteLine("Sending");
+        }
+        public static void HandleStop()
+        {
+            Processing = false;
+            Console.WriteLine("Stopping");
+        }
+    }
+}
---
+++ CardProcessor/Processor/YuGiOhCard.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Processor
+{
+    public class YuGiOhCard
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public string frameType { get; set; }
+        public string desc { get; set; }
+        public string race { get; set; }
+        public string archetype { get; set; }
+        //public List<CardSet> card_sets { get; set; }
+        public List<CardImage> card_images { get; set; }
+        //public List<CardPrice> card_prices { get; set; }
+    }
+
+    public class CardImage
+    {
+        public int id { get; set; }
+        public string image_url { get; set; }
+        public string image_url_small { get; set; }
+        public string image_url_cropped { get; set; }
+    }
+
+    //public class CardPrice
+    //{
+    //    public string cardmarket_price { get; set; }
+    //    public string tcgplayer_price { get; set; }
+    //    public string ebay_price { get; set; }
+    //    public string amazon_price { get; set; }
+    //    public string coolstuffinc_price { get; set; }
+    //}
+
+    //public class CardSet
+    //{
+    //    public string set_name { get; set; }
+    //    public string set_code { get; set; }
+    //    public string set_rarity { get; set; }
+    //    public string set_rarity_code { get; set; }
+    //    public string set_price { get; set; }
+    //}
+
+}
